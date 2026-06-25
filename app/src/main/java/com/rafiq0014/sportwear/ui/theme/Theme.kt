a0a7c08650e73d83644c91d86fa7ab336d693aa0
muhammad rafiq nur ghifari <rafiq121205@gmail.com>
2026-06-26 01:08:46 +0700

    fix unused import

--- app/src/main/java/com/rafiq0014/sportwear/ui/theme/Theme.kt
+++ app/src/main/java/com/rafiq0014/sportwear/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.rafiq0014.sportwear.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
